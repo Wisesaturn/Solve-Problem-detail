c202e2693af9abb17756b63df25960de514a2135
Jae Han Song <rfv1479@gmail.com>
2022-01-13 01:53:45 +0900

    게시물 레이아웃 width 및 padding 조정, footer 아이콘 추가

--- src/js/Footer/Footer.jsx
+++ src/js/Footer/Footer.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import '../../css/Footer.scss';
+import Github from '../../resource/Mark-Github.png';
+import Naver from '../../resource/Mark-naverBlog.png';
 
 function Footer() {
     return (
         <div className="Footer">
-           © 2022 Wisesaturn. Powered by Github with ReactJS <br/>
+           © 2022 Wisesaturn All Rights Reserved. Powered by Github with ReactJS. <br/>
            Thanks to Myself by 맨 땅에 헤딩
            <div className="Wrap_SNS">
                 <div className="SNS">
-                    깃허브
+                    <img src={Github}
+                        className="image"
+                        alt="Github"
+                        width="36"
+                        height="36"/>
                 </div>
                 <div className="SNS">
-                    블로그
+                    <img src={Naver}
+                        id="naver"
+                        className="image"
+                        alt="Naver"
+                        width="29"
+                        height="29" />
                 </div>
                 <div className="SNS">
                     포트폴리오
