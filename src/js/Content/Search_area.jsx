cb8ab723d1dcaab059982f636ee27b1fd20ea4fe
Jae Han Song <rfv1479@gmail.com>
2022-01-17 22:43:02 +0900

    검색바 디자인 완성

--- src/js/Content/Search_area.jsx
+++ src/js/Content/Search_area.jsx
@@ -2,30 +2,45 @@ import React from 'react';
 import styled from 'styled-components';
 import { search_active } from '../../js/state';
 import { useRecoilState } from 'recoil';
+import { AiOutlineEnter } from 'react-icons/ai'
 
 const Search_area = () => {
     const [active, setActive] = useRecoilState(search_active);
 
     return (
         <Search_area_wrap active={active}>
-            여기는 검색 영역입니다.
+            <input type="text" placeholder="검색어를 입력하세요"></input>
+            <AiOutlineEnter id="enter"/>
         </Search_area_wrap>
     )
 }
 
 const Search_area_wrap = styled.div`
-    display : ${ (props) => props.active ? 'block' : 'none' };
+    display : ${ (props) => props.active ? 'flex' : 'none' };
     position : relative;
     margin : 0 auto;
-    top : 2rem;
-    width : 30vw;
-    padding : 1.2vh 3vw;
     border 1px solid var(--search-border);
     border-radius : 5px;
 
     animation : ${ (props) => props.active
                         ? 'Search_open .4s ease-out forwards'
-                        : 'null' }
+                        : 'null' };
+    
+    > input {
+        display : block;
+        width : 30vw;
+        padding : 1.2vh 3vw;
+        background-color : inherit;
+        color : inherit;
+        border : none;
+    }
+
+    > #enter {
+        position : relative;
+        margin : auto 0;
+        padding : 0 .7vw;
+        display : block;
+    }
 `
 
 export default Search_area;
