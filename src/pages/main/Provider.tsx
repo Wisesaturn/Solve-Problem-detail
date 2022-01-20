68c9d2baedb67fcc48ba5be457fd147bc11057f8
Jae Han Song <rfv1479@gmail.com>
2022-01-20 09:49:31 +0900

    Feature #6 :: Provider.tsx
    - {children}을 모두 불러와 <div> 태그를 적용시키는 방법
    - 'body'처럼 적용, 다크모드용
    * Find #6

---
+++ src/pages/main/Provider.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { darkMode_state } from '../../components/state';
+import { useRecoilValue } from 'recoil';
+
+export default ({ children }) => {
+    const [prevent, setPrevent] = React.useState(false);
+    const check = useRecoilValue(darkMode_state);
+
+    React.useEffect(() => {
+        setPrevent(true);
+    }, []);
+
+    const body = <div id="App" className={`${check ? 'dark' : 'light'}`}>{children}</div>
+
+    // prevents ssr flash for mismatched dark mode
+    if (!prevent) {
+        return <div style={{ visibility: 'hidden' }}>{body}</div>
+    }
+
+    return body
+}
