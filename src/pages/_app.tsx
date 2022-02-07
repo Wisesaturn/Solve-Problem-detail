245a1f7c90e2f0dbd1f294fa1c3f05e684061516
Jae Han Song <rfv1479@gmail.com>
2022-02-08 01:08:08 +0900

    Update :: add initial Loading Component
    - Loading Component를 추가했습니다 (추후 디자인 예정)
    - 참고 :
      https://www.bhagyamudgal.me/blog/how-to-make-custom-loading-screen-in-nextjs-project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,34 @@
 import 'prismjs/themes/prism-okaidia.css'
 import '../../public/App.scss'
+import React, { useEffect, useState } from 'react'
 import { AppProps } from 'next/app'
 import Footer from '../components/Footer/Footer'
 import Head from 'next/head'
 import Header from '../components/Header/Header'
+import Loading from '../components/Loading'
 import Provider from './provider'
-import React from 'react'
 import { RecoilRoot } from 'recoil'
+import { useRouter } from 'next/router'
 
 function App({ Component, pageProps }: AppProps) {
+    // 로딩 페이지 만들기용 --------------
+    const router = useRouter()
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+    const handleStart = (url : string) => {
+      url !== router.pathname ? setLoading(true) : setLoading(false)
+      // useEffect로 변화가 생길 때, url 값이 변할 경우 Loading State를 true로 반환, 그렇지 않으면 false 반환
+    }
+    const handleComplete = (url : string) => setLoading(false)
+     // useEffect로 변화가 생길 때, url 값과 상관 없이 Loading State를 false로 반환
+
+    router.events.on('routeChangeStart', handleStart) // routeChange가 시작할 때 handleStart 함수를 통해 url 체크 (true/false)
+    router.events.on('routeChangeComplete', handleComplete) // routeChange가 완료될 때 handleComplete 함수를 통해 false 반환
+    router.events.on('routeChangeError', handleComplete) // routeChange가 오류가 났을 때 handleComplete 함수를 통해 false 반환
+  }, [router])
+  // 참고 : https://www.bhagyamudgal.me/blog/how-to-make-custom-loading-screen-in-nextjs-project
+
     return (
         <RecoilRoot>
             <Provider>
@@ -16,6 +36,7 @@ function App({ Component, pageProps }: AppProps) {
                     <title>재한쓰의 공부용 블로그 😊</title>
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 </Head>
+                <Loading loading={loading}/>
                 <Header />
                 <Component {...pageProps} />
                 <Footer />
