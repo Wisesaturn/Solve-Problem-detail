1b6c50d2c4a0ea3a530e7955b995ff230f37ca2e
Jae Han Song <rfv1479@gmail.com>
2022-02-08 09:47:49 +0900

    UpDate :: Delete Loading component
    - 일단 디자인적으론 progress bar가 더 나아서 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,15 @@
 import 'prismjs/themes/prism-okaidia.css'
 import '../../public/App.scss'
-import React, { useEffect, useState } from 'react'
 import { AppProps } from 'next/app'
 import Footer from '../components/Footer/Footer'
 import Head from 'next/head'
 import Header from '../components/Header/Header'
-import Loading from '../components/Loading'
 import NextNProgress from 'nextjs-progressbar'
 import Provider from './provider'
+import React from 'react'
 import { RecoilRoot } from 'recoil'
-import { useRouter } from 'next/router'
 
 function App({ Component, pageProps }: AppProps) {
-    // 로딩 페이지 만들기용 --------------
-    const router = useRouter()
-    const [loading, setLoading] = useState(false)
-
-    useEffect(() => {
-    const handleStart = (url : string) => {
-      url !== router.pathname ? setLoading(true) : setLoading(false)
-      // useEffect로 변화가 생길 때, url 값이 변할 경우 Loading State를 true로 반환, 그렇지 않으면 false 반환
-    }
-    const handleComplete = () => setLoading(false)
-     // useEffect로 변화가 생길 때, Loading State를 false로 반환
-
-    router.events.on('routeChangeStart', handleStart) // routeChange가 시작할 때 handleStart 함수를 통해 url 체크 (true/false)
-    router.events.on('routeChangeComplete', handleComplete) // routeChange가 완료될 때 handleComplete 함수를 통해 false 반환
-    router.events.on('routeChangeError', handleComplete) // routeChange가 오류가 났을 때 handleComplete 함수를 통해 false 반환
-  }, [router])
-  // 참고 : https://www.bhagyamudgal.me/blog/how-to-make-custom-loading-screen-in-nextjs-project
 
     return (
         <RecoilRoot>
